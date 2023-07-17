8c754645c36aaf8283872f8dd5efc280ed862d57
bariskarahan <bariskarahan99@yahoo.com>
2023-07-17 21:53:32 +0300

    update

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
+const clearBtn = document.getElementById('clear');
+const itemFilter = document.getElementById('filter');
+
 
 function addItem(e) {
     e.preventDefault();
@@ -19,9 +22,11 @@ function addItem(e) {
     li.appendChild(button);
 
     itemList.appendChild(li);
+    checkUI();
+
     itemInput.value = '';
 }
-git
+
 function createButton(classes) {
     const button = document.createElement('button')
     button.className = classes;
@@ -38,10 +43,37 @@ function createIcon(classes) {
 
 function removeItem(e) {
     if (e.target.parentElement.classList.contains('remove-item')) {
-        e.target.parentElement.parentElement.remove();
+        if (confirm('Are you sure?')) {
+            e.target.parentElement.parentElement.remove();
+
+            checkUI();
+        }
     }
 }
 
+function clearItems() {
+    while (itemList.firstChild) {
+        itemList.removeChild(itemList.firstChild);
+    }
+
+    checkUI();
+    // itemList.innerHTML = '';
+}
+
+function checkUI() {
+const items = itemList.querySelectorAll('li');
+
+    if (items.length === 0) {
+        clearBtn.style.display = 'none';
+        itemFilter.style.display = 'none';
+    } else {
+        clearBtn.style.display = 'block';
+        itemFilter.style.display = 'block';        
+    }
+}
+
+checkUI();
+
 itemForm.addEventListener('submit', addItem);
 itemList.addEventListener('click', removeItem);
-
+clearBtn.addEventListener('click' , clearItems);
