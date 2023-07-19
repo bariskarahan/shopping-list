ead576d27f9547751fe550e460aa10ef81c1a9fb
bariskarahan <bariskarahan99@yahoo.com>
2023-07-19 18:39:10 +0300

    color

--- script.js
+++ script.js
@@ -203,3 +203,25 @@ function init() {
 }
 
 init();
+
+
+let intervalID;
+
+function startChange() {
+    if (!intervalID) {
+        intervalID = setInterval(changeRandomColor , 750);
+    }
+}
+
+function changeRandomColor() {
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    document.body.style.backgroundColor = `#${randomColor}`;
+
+}
+
+function stopChange() {
+    clearInterval(intervalID);
+}
+
+document.getElementById('start').addEventListener('click' , startChange)
+document.getElementById('stop').addEventListener('click' , stopChange)
